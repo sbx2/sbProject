ed812959d3f26c9b743ae676c69cdabcad5ecbbf
zhhrozhh <zhangh40@msu.edu>
2016-10-08 17:30:54 -0400

    calc/addpd/setval

--- zhhVersion/csvp.py
+++ zhhVersion/csvp.py
@@ -18,9 +18,26 @@ def readCSV(fn):
                 except:continue
             try:dt[row[1]] = tem
             except:continue
-        res[wt] = dt        
+        res['header'] = header[2:]
+        res[wt] = dt
     f.close()
-
     return res
+def addPD(table,wt,pd):
+    table[wt][pd] = {}
+    for i in table['header']:
+        table[wt][pd][i] = ''
+def setVal(table,wt,pd,dt,val):
+    table[wt][pd][dt] = val
+def isNum(x):
+    try:int(x)
+    except:return 0
+    return 1
+def calcNoWs(table,wt,pd):
+    table[wt][pd]['# of wheels']=sum([int(table[wt][pd][d]) if d!='# of wheels' and isNum(table[wt][pd][d]) else 0 for d in table[wt][pd]])
+
+
 r = readCSV('../Deburr.csv')
-print(r['C12']['344']['14-Sep'])
+addPD(r,'C02','111')
+setVal(r,'C02','111','8-Sep','26')
+calcNoWs(r,'C02','346')
+print(r['C02']['346']['# of wheels'] )
