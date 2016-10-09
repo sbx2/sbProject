cdf7d1e2530927227244c47475a7f400603e8e3d
zhhrozhh <zhangh40@msu.edu>
2016-10-09 09:59:10 -0400

    almost there

--- zhhVersion/csprReVer.py
+++ zhhVersion/csprReVer.py
@@ -8,6 +8,7 @@ class CSreportController:
     def __init__(self,fn,dli = 477):
         f = open(fn)
         t = f.read().split(chr(dli))
+        self.wtList = []
         self.res = {}
         for table in t:
             trs = table.split('\n')
@@ -16,15 +17,17 @@ class CSreportController:
             dt = {}
             for row in trs[1:]:
                 row = [num(x) for x in row.split(',')]
-                #row = row.split(',')
                 tem = {}
                 for i in range(2,len(header)):
                     try:tem[header[i]]=row[i]
                     except:continue
                 try:dt[row[1]] = tem
                 except:continue
-            self.header = header[3:]
+            self.header = header[4:]
             self.res[wt]=dt
+            self.wtList.append(wt)
+        self.wtList.sort()
+        self.header.sort(key = lambda x:int(x.split('-')[0]))
         f.close()
     def addPD(self,wt,pd):
         self.res[wt][pd] = {}
@@ -32,11 +35,35 @@ class CSreportController:
             self.res[wt][pd][i] = 0
     def setVal(self,wt,pd,dt,val):
         self.res[wt][pd][dt] = val
-    def calcNoW(self,wt,pd):
-        self.res[wt][pd]['# of wheels'] = sum([self.res[wt][pd][d] for d in self.header])
+    def calcTotalW(self):
+        for wt in self.res:
+            for pd in self.res[wt]:
+                self.res[wt][pd]['# of wheels'] = sum([self.res[wt][pd][d] for d in self.header])
+    def calcTotalD(self):
+        for wt in self.res:
+            self.res[wt]['dailyTotal'] = {}
+            for h in self.header:
+                self.res[wt]['dailyTotal'][h] = sum([self.res[wt][pd][h] if pd !='dailyTotal' else 0 for pd in self.res[wt]])
+    def oup(self,fn,dli = ''):
+        f = open(fn,'w')
+        for wt in self.wtList:
+            row0 = dli+wt+',Problem Description,# of wheels,Ratio,'+','.join(self.header)
+            rows = []
+            pdList = [pd if type(pd)==int else None for pd in self.res[wt]]
+            for pd in pdList:
+                try:rows.append([self.res[wt][pd]['# of wheels'],','+str(pd)+','+str(self.res[wt][pd]['# of wheels'])+','+str(self.res[wt][pd]['Ratio'])+','+','.join([str(self.res[wt][pd][h]) for h in self.header])])
+                except:continue
+            rows.sort(key = lambda x:x[0],reverse = True)
+            rows = [row[1] for row in rows]
+            f.writelines(row0+'\n')
+            f.writelines('%s\n'%line for line in rows)
+            datot = [self.res[wt]['dailyTotal'][h] for h in self.header]
+            rowf = ',Total,'+str(sum(datot))+','+','.join([str(x) for x in datot])
+            f.writelines(rowf+'\n')
+        f.close()
 
 a = CSreportController('../DeBurr.csv')
-a.addPD('C02',1000)
-a.setVal('C02',1000,a.header[3],231)
-for x in a.res['C02']:
-    print(x,a.res['C02'][x])
+a.calcTotalW()
+a.calcTotalD()
+print(a.header)
+a.oup('oup.csv')
