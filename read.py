d74cb530a34a81cceeebd95c0cebc7d79d31a63a
philphy <derek.wzy@gmail.com>
2016-10-12 14:48:27 -0400

    dictionary
    字典大概完成吧

--- read.py
+++ read.py
@@ -21,15 +21,15 @@ def zidian():
     c02pd=[]
     c02wt = rd('Deburrcopy.csv')[0][0]
     dt = rd('Deburrcopy.csv')[0][4:]
-    for i in range(1,c02cln):
+    for i in range(1,c02cln):   #PD column
         c02pd.append(rd('Deburrcopy.csv')[i][1])
-    for i in range(1,c02cln):
+    for i in range(1,c02cln):   # "# of wheels" total number for c02
         c02ttnum.append(rd('Deburrcopy.csv')[i][2])
-    dictc02 = {c02wt:c02pd}
-    for i in range(0,c02cln-1):
+    dictc02 = {c02wt:c02pd}     #dictionary
+    for i in range(0,c02cln-1): # {'C02':[346..352]}
         dictc02[c02pd[i]] = c02ttnum[i]
-    for i in range(1,c02cln-1):
-        dictc02[c02ttnum[i]] = sum(rd('Deburrcopy.csv')[i][4],rd('Deburrcopy.csv')[i][33])
+    for i in range(1,c02cln-1): # {total :[all date]}
+        dictc02[c02ttnum[i-1]] = rd('Deburrcopy.csv')[i][4:]
     return dictc02
 
 
