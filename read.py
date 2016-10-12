6181a3509f846843bac9193ab06d81835a10fea4
philphy <derek.wzy@gmail.com>
2016-10-12 11:05:31 -0400

    dict
    字典？

--- read.py
+++ read.py
@@ -1,24 +1,27 @@
 import csv
+from Date import Date
 #open
 def isNum(x):
     try:int(x)
     except:return 0
     return 1
+
+# char(67) = 'C'
 def rd(fn):
     res = []
     with open(fn, newline='\n') as csvin:
-        c02data = csv.reader(csvin, delimiter=',')
-        for row in c02data:
-            res.append([int(x) if isNum(x) else x for x in row ])
-    return res
+        data = csv.reader(csvin, delimiter=',')
+        for row in data:
+            res.append([int(x) if isNum(x) else x for x in row])
+        return res
 
-            
-#def wt():
-#    with open('testout.csv', 'w') as csvout:
-#        writer = csv.writer(csvout);
-#        for row in writer:
-#            writer.writerow(row+['FFF']);
-#    return;
+def zidian():
+    c02wt = rd('Deburrcopy.csv')[0][0]
+    dt = rd('Deburrcopy.csv')[0][4:]
+    for i in range(1,14):
+        pd = pd.append(rd('Deburrcopy.csv')[i][1]
+    return pd
 
-y = rd('test.csv')
-print(y[0][0])
+
+#print(rd('Deburrcopy.csv')[0][0])
+print(zidian())
